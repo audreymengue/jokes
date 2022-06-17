8f880931a7ad9949624696a844f8434305d3ad17
Audrey Mengue <massoumeharmonie@gmail.com>
2022-06-17 00:06:26 +0000

    Testing deployment

--- src/Jokes.js
+++ src/Jokes.js
@@ -5,6 +5,7 @@ const Jokes = ({ data }) => {
     return (
       <article className="joke">
         <div className="single-joke">
+        <p>It's just one joke at a time.</p>
           <h3>{data.joke}</h3>
         </div>
            
