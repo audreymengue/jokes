3240284539da850cb7433965062e05fc50888d36
Audrey Mengue <massoumeharmonie@gmail.com>
2022-10-25 08:54:23 +0000

    Implemented mixpanel

--- src/App.js
+++ src/App.js
@@ -1,26 +1,28 @@
 import { useEffect, useState } from "react";
 import Jokes from "./Jokes";
 import Setup from "./setup";
+import mixpanel from "mixpanel-browser";
+mixpanel.init(process.env.REACT_APP_MIXPANEL)
+
 
 const url = "https://v2.jokeapi.dev/joke/Programming";
 
 function App() {
-const [loading, setLoading] = useState(true);
-const [data, setData] = useState([]);
+  const [loading, setLoading] = useState(true);
+  const [data, setData] = useState([]);
 
-    const fetchJokes = async () =>  {
-      setLoading(true)
-      try {
-        const response = await fetch(url);
-        const data = await response.json();
-        setLoading(false);
-        setData(data)
-       
-      } catch (error) {
-        setLoading(false);
-        console.log(error);
-      }
+  const fetchJokes = async () => {
+    setLoading(true);
+    try {
+      const response = await fetch(url);
+      const data = await response.json();
+      setLoading(false);
+      setData(data);
+    } catch (error) {
+      setLoading(false);
+      console.log(error);
     }
+  };
 
   useEffect(() => {
     fetchJokes();
@@ -29,13 +31,26 @@ const [data, setData] = useState([]);
   return (
     <div className="App">
       <main>
-        <h1 className="smile">Your daily dev smile from jokeAPI <span>:) :)</span></h1>
-      { data.joke ? <Jokes loading={loading} data={data}/> 
-        : data.setup ? <Setup loading={loading} data={data}/>
-        : <p>No jokes available</p>}
+        <h1 className="smile">
+          Your daily dev smile from jokeAPI <span>:) :)</span>
+        </h1>
+        {data.joke ? (
+          <Jokes loading={loading} data={data} />
+        ) : data.setup ? (
+          <Setup loading={loading} data={data} />
+        ) : (
+          <p>No jokes available</p>
+        )}
       </main>
-        <button onClick={fetchJokes} className="btn">Click and smile</button>
-        <footer> <p>More jokes at <a href="https://sv443.net/jokeapi/v2">jokeAPI</a></p> </footer>
+      <button onClick={fetchJokes} className="btn">
+        Click and smile
+      </button>
+      <footer>
+        {" "}
+        <p>
+          More jokes at <a href="https://sv443.net/jokeapi/v2">jokeAPI</a>
+        </p>{" "}
+      </footer>
     </div>
   );
 }
